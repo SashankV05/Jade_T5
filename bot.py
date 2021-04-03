f2ce8e1f47d405bf41e637c3570ae5a39a2906ec
Sashank <32218136+SashankV05@users.noreply.github.com>
2021-04-03 08:57:07 +0530

    removed commands from on_message and made seperate handlers

--- bot.py
+++ bot.py
@@ -7,8 +7,9 @@ from datetime import datetime
 import time
 
 intents = discord.Intents(messages=True, guilds=True, typing = False, presences = False, members=False)
-client = discord.AutoShardedClient(intents=intents, chunk_guilds_at_startup=False)
 config_general=json.loads(open("config-bot.json","r").read())
+client = discord.AutoShardedClient(intents=intents, chunk_guilds_at_startup=False,command_prefix=str(config_general["prefix"]))
+
 
 model=ResponseGenerator("https://woz-model.herokuapp.com/v1/models/jade:predict","https://discord.com/api/webhooks/806950915449683970/-8IG5UkdBGf7jgfQ36XlfRSIUjt2V-rt-RNn9NdC3zDgfvjzMS2SEMj-XlozsXH9Ovju")
 
@@ -25,16 +26,24 @@ async def on_message(message):
     loop = asyncio.get_event_loop()
     if message.author.bot == False and message.guild != None:
         if message.content.lower().startswith(config_general["prefix"]):
-            message.content=message.content[len(config_general["prefix"]):]
-            if message.content == "-h":
-                logs=model.register(message.author.id, time.time())
-                await message.reply("History:\n> "+"\n> ".join(logs["history"])+f"\nLast seen: {datetime.fromtimestamp(logs['timestamp'])}")
-            elif message.content == "-r":
-                model.reset(message.author.id)
-                await message.reply("Successfully reset your history with Jade")
-            else:
-                async with message.channel.typing():
-                    out = await loop.run_in_executor(ThreadPoolExecutor(), model.response, message.author, message.content, True)
+            msg=message.content
+            tomodel_message=msg.replace("jd","")
+            async with message.channel.typing():
+                    out = await loop.run_in_executor(ThreadPoolExecutor(), model.response, message.author, tomodel_message, True)
                 await message.reply(out.replace("@everyone","").replace("@here", ""))
+                                   
+                                    
+@bot.command()
+async def reset(ctx):
+    model.reset(message.author.id)
+    await message.reply("Successfully reset your history with Jade")
+                                    
+@bot.command()
+async def history(ctx):
+    ogs=model.register(message.author.id, time.time())
+    await message.reply("History:\n> "+"\n> ".join(logs["history"])+f"\nLast seen: {datetime.fromtimestamp(logs['timestamp'])}")
+
+
+
 
 client.run(config_general["token"])
